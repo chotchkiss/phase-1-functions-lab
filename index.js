cf19d254c5a80fc41dc678255f548640e74c9daf
Cheyenne Hotchkiss <cheycheymh@gmail.com>
2023-10-31 18:20:26 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(feet){
+    return distanceFromHqInBlocks(feet)*264
+}
+function distanceTravelledInFeet(end, start){
+    return Math.abs((end - start) * 264)
+}
+
+function calculatesFarePrice(end, start) {
+    let distance = distanceTravelledInFeet(end, start);
+    if (distance > 2500) {
+        return "cannot travel that far";
+    } else if (distance > 2000) {
+        return 25;
+    } else if (distance > 400) {
+        return (distance - 400) * 0.02;
+    } else {
+        return 0;
+    }
+}
